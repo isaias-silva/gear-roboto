7c4b91ed48d172ac915ab64d51bced96271b27e7
isaias santos <84031593+isaias-silva@users.noreply.github.com>
2025-06-22 13:07:46 -0300

    fix: :bug: CommandLine transporter fix (#23)

--- src/core/transporters/CommandLineTransporter.ts
+++ src/core/transporters/CommandLineTransporter.ts
@@ -1,3 +1,4 @@
+import { TransportMessage } from "../../interfaces";
 import { IFlowResponse } from "../../interfaces/IFlowResponse";
 import { IMessageConnection } from "../../interfaces/IMessageConnection";
 import { IMessageReceived } from "../../interfaces/IMessageReceived";
@@ -5,20 +6,20 @@ import { DefaultTransporter } from "./DefaultTransporter";
 
 export class CommandLineTransporter extends DefaultTransporter {
 
-    protected treatInfoConn(msg: IMessageConnection): void {
+    protected treatInfoConn(msg: TransportMessage<IMessageConnection>): void {
         const { adInfo } = msg;
-        adInfo.forEach((v, k) => console.log(`${k} - ${v}`))
+        adInfo?.forEach((v, k) => console.log(`${k} - ${v}`))
 
     }
-    protected treatInfoMsg(msg: IMessageReceived): void {
+    protected treatInfoMsg(msg: TransportMessage<IMessageReceived>): void {
         const { text, author } = msg
         console.log(`[${author}]: ${text}`)
     }
-    protected treatInfoFlow(msg: IFlowResponse): void {
+    protected treatInfoFlow(msg: TransportMessage<IFlowResponse>): void {
         const { messages, chatId } = msg;
 
         console.log(`\n[Responses from "${chatId}"]\n`);
 
-      
+
     }
 }
